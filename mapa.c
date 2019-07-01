9082464b5cdb9b33eed98e451f62973fd3af1515
giseleml <giselemaraleo@gmail.com>
2019-07-01 16:31:09 -0300

    Grande Progresso das Funcionalidades =)

--- mapa.c
+++ mapa.c
@@ -1,51 +1,56 @@
 //Atividade MAPA - ALP II
-
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXIMO 5 
+#include <locale.h>
+
+#define maximo 5
 
 int option; 
-int codigo;
-char titulo[50];
-char autor[50];
-char editora[50];
-
-int main(void) {
-  printf("** Menu de Opções ** \n Digite 1 para cadastrar um novo livro;\n Digite 2 para exibir os livros cadastrados; \n Digite 0 para sair");
-  //system pause
-  scanf("%d", &option);
-  if (option == 1) {
-    printf("** Menu de Cadastro ** \n");
-    printf("Informe o título do livro: \n");
-    scanf("%s", titulo);
-    printf("Informe o nome do(a) autor(a): \n");
-    scanf("%s", autor);
-    printf("Informe o nome da editora: \n");
-    scanf("%s", editora);
-    //Gera um número aleatório
-    codigo = rand() % 5;
-    printf("titulo %s, autor %s, editora %s codigo %d", titulo, autor, editora, codigo);
-  } else {
-    if (option == 2) {
-      printf("** Livros Cadastrados ** \n");
-      printf("titulo %s, autor %s, editora %s codigo %d", titulo, autor, editora, codigo);
-    } else {
-      printf("*Nada*");
-    }
-  }
-  return 0;
-}
-
-
-/* int arr[5];
 int i;
 
+typedef struct {
+	int codigo;
+	char titulo[50];
+    char autor[50];
+    char editora[50];	
+} cadastro;
+
 int main(void) {
-  for (i = 0; i < 5; i++) {
-    printf("Enter number %d", i + 1);
-    scanf("%d", &arr[i]);
-  }
-  printf("%d array %d", arr[1], arr);
-  return 0;
+  cadastro livro[maximo];
+  setlocale(LC_ALL, "Portuguese");
+  
+  do {
+   printf("** Menu de Opções ** \n Digite 1 para cadastrar um novo livro;\n Digite 2 para exibir os livros cadastrados; \n Digite 0 para sair \n \n");
+   scanf("%d", &option);
+   system("cls");
+   
+	  switch(option) {
+	  	case 1: 
+		  	for (i = 0; i < maximo; i++) {
+		  	 printf("** Menu de Cadastro ** \n \n");
+		     printf("Informe o título do livro %d: \n", i + 1);
+		     scanf(" %[^\n]", &livro[i].titulo);
+		     fflush(stdin);
+		     printf("Informe o nome do(a) autor(a): \n");
+		     scanf(" %[^\n]", &livro[i].autor);
+		     fflush(stdin);
+		     printf("Informe o nome da editora: \n");
+		     scanf(" %[^\n]", &livro[i].editora);
+		     fflush(stdin);
+		     livro[i].codigo = i + 1;
+			}
+			system("cls");
+		    break;
+		case 2:
+			printf("** Livros Cadastrados ** \n");
+		    for (i = 0; i < maximo; i++) {
+		    	printf("Código do Livro: %d, \n Título: %s, \n Autor: %s, \n Editora: %s \n \n", livro[i].codigo, livro[i].titulo, livro[i].autor, livro[i].editora);
+			}
+		    break;
+		default: 
+			printf("** Opcão Inválida *");
+			break;
+	  }
+	} while (option != 0);
 }
-*/
+
